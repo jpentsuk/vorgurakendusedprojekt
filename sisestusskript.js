564ef70a7c3e8d07b7ec4730c6818730e64fac61
jpentsuk <jpentshuk@gmail.com>
2017-04-28 15:28:52 +0300

    Uuendatud andmete sisestamise vormi, lisatud javaskripti

---
+++ sisestusskript.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jpent on 28.04.2017.
+ */
+window.onload = function()
+{
+    var muudatekstisuurust=document.getElementById("sisestused");
+    muudatekstisuurust.onmouseover = function () {
+        muudatekstisuurust.style.fontSize='18px';
+    }
+
+    muudatekstisuurust.onmouseout = function () {
+        muudatekstisuurust.style.fontSize='14px';
+    }
+
+}
